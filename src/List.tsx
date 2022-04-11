2eac7335982101321f65fdd2862251f66a68c1b1
loknarbLaptop <66973931+loknarb@users.noreply.github.com>
2022-04-11 10:14:13 +0200

    pass props down with interface type

--- src/List.tsx
+++ src/List.tsx
@@ -1,5 +1,12 @@
-const List = ()=> {
-  return ( <div></div> );
-}
+import { ListItem } from "./types";
+const List: React.FunctionComponent<{ items: ListItem[] }> = ({ items }) => {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{item.name}</li>
+      ))}
+    </ul>
+  );
+};
 
 export default List;
