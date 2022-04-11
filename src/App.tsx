023a54b062737366947d99749c5f5a98c1f5e068
loknarbLaptop <66973931+loknarb@users.noreply.github.com>
2022-04-11 09:46:00 +0200

    array of objects pass down as children props into List

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
-import './App.css';
-import Box from './Box';
-const Body = ({title}: {title: string}) => {
-  return (
-    <h5>{title}</h5>
-  )
-}
+import "./App.css";
+import Box from "./Box";
+import List from "./List";
+const MockData = [
+  {
+    name: "Ocean Eyes",
+  },
+  {
+    name: "Mine",
+  },
+  {
+    name: "Forgot about Dre",
+  },
+  {
+    name: "Gods Plan",
+  },
+];
+const Body = ({ title }: { title: string }) => {
+  return <h5>{title}</h5>;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
-      <Body title="Body Title"/>
+      <header className="App-header">Hello</header>
+      <Body title="Body Title" />
       <Box>Children Here</Box>
+      <List>{MockData}</List>
     </div>
   );
 }
