a5a3e0dbf721df73566c0abbdb04fcbc5b3b7290
loknarbLaptop <66973931+loknarb@users.noreply.github.com>
2022-04-11 17:20:41 +0200

    fetching mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 // import Box from "./Box";
 import List from "./List";
@@ -17,15 +17,20 @@ const MockData: ListItem[] = [
     name: "Gods Plan",
   },
 ];
-const Body = ({ title }: { title: string }) => {
-  return <h5>{title}</h5>;
-};
+// const Body = ({ title }: { title: string }) => {
+//   return <h5>{title}</h5>;
+// };
 
 function App() {
   const onListClickHandler = useCallback((item: ListItem) => {
     alert(item.name);
   }, []);
   const [payload, setPayload] = useState<Payload | null>(null);
+  useEffect(() => {
+    fetch("/public/data.json")
+      .then((response) => response.json())
+      .then((data) => setPayload(data));
+  }, []);
   return (
     <div className="App">
       {/* <header className="App-header">Hello</header> */}
