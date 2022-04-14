f3a33c526611f56fba794215cfe81b9f66fdb728
loknarbLaptop <66973931+loknarb@users.noreply.github.com>
2022-04-14 21:02:05 +0200

    todo interface init

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,14 @@
 export interface ListItem {
   name: string;
 }
+
+export interface Payload {
+  name: string;
+}
+
+export interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+export type ActionType = { type: "ADD"; text: string } | { type: "DELETE"; id: number };
