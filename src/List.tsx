838d18835fc32e0dbe139322ea4fb175044c071d
loknarbLaptop <66973931+loknarb@users.noreply.github.com>
2022-04-15 15:15:16 +0200

    index removed, passed down item.id as key

--- src/List.tsx
+++ src/List.tsx
@@ -11,14 +11,14 @@ const List: React.FunctionComponent<{
       style={{
         listStyleType: "none",
       }}>
-      {items.map((item, index) => (
+      {items.map((item) => (
         // <li key={index} onClick={() => onClick?.(item)}>
         //   {item.text}
         //   <button className="btn-delete" onClick={() => onDelete?.(index)}>
         //     Delete
         //   </button>
         // </li>
-        <ListItem key={index} onDelete={() => onDelete?.(index)} text={item.text} />
+        <ListItem key={item.id} onDelete={() => onDelete?.(item.id)} text={item.text} />
       ))}
     </ul>
   );
