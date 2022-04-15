e93966cf735c19fef4e64c6f393521f5d21b5121
loknarbLaptop <66973931+loknarb@users.noreply.github.com>
2022-04-15 15:16:30 +0200

    listitem passed down

--- src/ListItem.tsx
+++ src/ListItem.tsx
@@ -1,5 +1,20 @@
-const ListItem = () => {
-  return (  );
-}
+import { Todo } from "./types";
+import "./App.css";
+import CheckBoxEmpty from "./CheckBoxEmpty";
+const ListItem: React.FunctionComponent<{
+  key: Todo["id"];
+  onDelete: (id: Todo["id"]) => void;
+  text: Todo["text"];
+}> = ({ text, onDelete, key }) => {
+  return (
+    <li>
+      <CheckBoxEmpty />
+      {text}
+      <button onClick={() => onDelete(key)} className="btn-delete">
+        Delete
+      </button>
+    </li>
+  );
+};
 
-export default ListItem ;
+export default ListItem;
