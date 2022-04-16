2eb068de00a3cd9f1746b46b602fcbb6b760a2a9
loknarbLaptop <66973931+loknarb@users.noreply.github.com>
2022-04-16 12:33:13 +0200

    check boolean and checktoggle passed down

--- src/ListItem.tsx
+++ src/ListItem.tsx
@@ -1,14 +1,21 @@
 import { Todo } from "./types";
 import "./App.css";
 import CheckBoxEmpty from "./CheckBoxEmpty";
+import CheckBoxFilled from "./CheckBoxFilled";
 const ListItem: React.FunctionComponent<{
   key: Todo["id"];
   onDelete: (id: Todo["id"]) => void;
+  onCheck: (id: Todo["id"]) => void;
   text: Todo["text"];
-}> = ({ text, onDelete, key }) => {
+  completed: Todo["completed"];
+}> = ({ text, onDelete, onCheck, key, completed }) => {
   return (
     <li>
-      <CheckBoxEmpty />
+      {completed ? (
+        <CheckBoxEmpty onCheck={onCheck(key)} />
+      ) : (
+        <CheckBoxFilled onCheck={onCheck(key)} />
+      )}
       {text}
       <button onClick={() => onDelete(key)} className="btn-delete">
         Delete
