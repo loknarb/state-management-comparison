32580bacfd6f5254f9ffe3906bef10cc46072ff0
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-08 16:08:52 +0200

    removed props pass thru

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,30 +1,20 @@
-import { Todo } from "../types/types";
 import "../styles/App.css";
 import ListItem from "./ListItem";
 import { useContext } from "react";
 import { TodosContext } from "./context/todoContext";
-const List = ({
-  onCheck,
-  onDelete,
-}: React.DetailedHTMLProps<React.HTMLAttributes<HTMLUListElement>, HTMLUListElement> & {
-  onCheck: (id: Todo["id"]) => void;
-  onDelete: (id: Todo["id"]) => void;
-}) => {
+const List: React.FunctionComponent<
+  React.DetailedHTMLProps<React.HTMLAttributes<HTMLUListElement>, HTMLUListElement>
+> = ({ style, ...rest }) => {
   const { todos } = useContext(TodosContext);
   return (
     <ul
+      {...rest}
       style={{
+        ...style,
         listStyleType: "none",
       }}>
       {todos.map((todo) => (
-        <ListItem
-          key={todo.id}
-          id={todo.id}
-          onDelete={() => onDelete?.(todo.id)}
-          onCheck={() => onCheck?.(todo.id)}
-          text={todo.text}
-          completed={todo.completed}
-        />
+        <ListItem key={todo.id} id={todo.id} text={todo.text} completed={todo.completed} />
       ))}
     </ul>
   );
