eea6567c80c0f7f68c95f7af93514a0a6863595c
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-08 16:06:44 +0200

    removed last props from app and dependencies

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,14 @@
 import "../styles/App.css";
 import { TodosProvider } from "./context/todoContext";
-import { useTodos } from "./hooks/useTodos";
 import Input from "./Input";
 import List from "./List";
 
 function App() {
-  const { removeTodo, checkTodo } = useTodos([]);
   return (
     <div className="App">
       <TodosProvider initialTodo={[]}>
         <Input />
-        <List onDelete={removeTodo} onCheck={checkTodo} />
+        <List />
       </TodosProvider>
     </div>
   );
