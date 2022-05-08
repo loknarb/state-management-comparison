a47902465841dcce18c553a13bc22eec1793d982
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-08 16:10:57 +0200

    moved files

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./components/App";
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./tests/reportWebVitals";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
