54bebb334734f8335e1729d93e11d981a8dcc2bf
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-08 14:59:54 +0200

    removed reducer logic

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,52 +1,14 @@
-import { useReducer } from "react";
 import "../styles/App.css";
+import { useTodos } from "./hooks/useTodos";
 import Input from "./Input";
 import List from "./List";
-import { v4 as uuid } from "uuid";
-import { Todo, ActionType } from "../types/types";
-const todoReducer = (state: Todo[], action: ActionType) => {
-  switch (action.type) {
-    case "ADD":
-      return [...state, { id: uuid(), text: action.text, completed: false }];
-    case "DELETE":
-      return state.filter((todo) => todo.id !== action.id);
-    case "TOGGLE_CHECK":
-      let copyState = state.slice();
-      let updatedTodo: Todo;
-      const i = copyState.findIndex(({ id: copyStateId }) => copyStateId === action.id);
-      if (copyState[i].completed === true) {
-        updatedTodo = {
-          id: state[i].id,
-          text: state[i].text,
-          completed: false,
-        };
-      } else {
-        updatedTodo = {
-          id: state[i].id,
-          text: state[i].text,
-          completed: true,
-        };
-      }
-      copyState[i] = updatedTodo;
-      return [...copyState];
-  }
-};
 
 function App() {
-  const [todos, dispatch] = useReducer(todoReducer, []);
-  const addTodoHandler = (text: Todo["text"]) => {
-    dispatch({ type: "ADD", text });
-  };
-  const removeTodoHandler = (id: Todo["id"]) => {
-    dispatch({ type: "DELETE", id });
-  };
-  const checkButtonHandler = (id: Todo["id"]) => {
-    dispatch({ type: "TOGGLE_CHECK", id });
-  };
+  const { todos, addTodo, removeTodo, checkTodo } = useTodos([]);
   return (
     <div className="App">
-      <Input onAdd={addTodoHandler} />
-      <List items={todos} onDelete={removeTodoHandler} onCheck={checkButtonHandler} />
+      <Input onAdd={addTodo} />
+      <List items={todos} onDelete={removeTodo} onCheck={checkTodo} />
     </div>
   );
 }
