238c692bd2818c45cbcb21a9dd7752a4aca101cd
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-08 13:46:31 +0200

    remove comments

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,23 +7,6 @@ import List from "./List";
 import { v4 as uuid } from "uuid";
 // import { ListItem, Payload, Todo, ActionType } from "./types";
 import { Todo, ActionType } from "../types/types";
-// const MockData: ListItem[] = [
-//   {
-//     name: "Ocean Eyes",
-//   },
-//   {
-//     name: "Mine",
-//   },
-//   {
-//     name: "Forgot about Dre",
-//   },
-//   {
-//     name: "Gods Plan",
-//   },
-// ];
-// const Body = ({ title }: { title: string }) => {
-//   return <h5>{title}</h5>;
-// };
 const todoReducer = (state: Todo[], action: ActionType) => {
   switch (action.type) {
     case "ADD":
@@ -48,39 +31,11 @@ const todoReducer = (state: Todo[], action: ActionType) => {
         };
       }
       copyState[i] = updatedTodo;
-      // console.log("prev", copyState[i]);
-      // if (copyState[i].completed === true) {
-      //   console.log("targetted");
-      //   copyState[i].completed = false;
-      // } else if (copyState[i].completed === false) {
-      //   console.log("targetted");
-      //   copyState[i].completed = true;
-      // }
-      // copyState[i]["completed"] = !copyState[i].completed;
-      // console.log(copyState[i]["completed"]);
-      // const prevState = copyState.filter((todo) => todo.id !== action.id);
-      // const invObj = !copyState[i].completed;
-      // copyState[i].completed = true;
-      // state[i] = copyState[i];
-
-      // console.log("curr", copyState[i]);
-      // console.log("try copyState", state);
-      // console.log("copyState in Reducer", [...copyState]);
-      // console.log(todoItem);
       return [...copyState];
   }
 };
 
 function App() {
-  // const onListClickHandler = useCallback((item: Todo) => {
-  //   alert(item.text);
-  // }, []);
-  // const [payload, setPayload] = useState<Payload | null>(null);
-  // useEffect(() => {
-  //   fetch("/data.json")
-  //     .then((response) => response.json())
-  //     .then((data) => setPayload(data));
-  // }, []);
   const [todos, dispatch] = useReducer(todoReducer, []);
   const addTodoHandler = (text: Todo["text"]) => {
     dispatch({ type: "ADD", text });
