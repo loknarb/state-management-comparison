b6b94f942bf59750af186d54af3b8cf8747be137
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-08 21:54:44 +0200

    typed usedispatch implemented on add todo

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,11 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 import PrimaryButton from "./PrimaryButtons";
-import { TodosContext } from "./context/todoContext";
+import { useTypedDispatch } from "./hooks/useReduxHook";
+import { addTodo } from "../stores/todo-slice";
 const Input: React.FunctionComponent = () => {
-  const { addTodo } = useContext(TodosContext);
+  const dispatch = useTypedDispatch();
+  // const { addTodo } = useContext(TodosContext);
   const [inputValue, setInputValue] = useState("");
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue: string = e.target.value;
@@ -12,7 +14,8 @@ const Input: React.FunctionComponent = () => {
   const submitTodo = (e: React.FormEvent<HTMLElement>) => {
     e.preventDefault();
     if (inputValue?.trim() !== "" && inputValue !== null) {
-      addTodo(inputValue);
+      dispatch(addTodo(inputValue));
+      // addTodo(inputValue);
 
       setInputValue("");
     }
