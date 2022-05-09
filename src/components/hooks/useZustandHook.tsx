018f2f8ee2c6a65bbeba36c83cab36cc7f7a75be
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-09 12:49:35 +0200

    built initial zustand store

---
+++ src/components/hooks/useZustandHook.tsx
@@ -0,0 +1,37 @@
+import { v4 as uuid } from "uuid";
+import { Todo } from "../../types/types";
+import create from "zustand";
+const useTodos = create<{
+  todos: Todo[];
+  checkTodo: (id: Todo["id"]) => void;
+  addTodo: (text: Todo["text"]) => void;
+  removeTodo: (id: Todo["id"]) => void;
+}>((set) => ({
+  todos: [],
+  addTodo: (text: Todo["text"]) =>
+    set((state) => ({
+      todos: handleAddTodo(state.todos, text),
+    })),
+  removeTodo: (id: Todo["id"]) =>
+    set((state) => ({
+      todos: handleRemoveTodo(state.todos, id),
+    })),
+  checkTodo: (id: Todo["id"]) =>
+    set((state) => ({
+      todos: handleCheckTodo(state.todos, id),
+    })),
+}));
+
+const handleCheckTodo = (todos: Todo[], id: Todo["id"]): Todo[] => {
+  return todos.map((todo) => ({
+    ...todo,
+    completed: todo.id === id ? !todo.completed : todo.completed,
+  }));
+};
+const handleRemoveTodo = (todos: Todo[], id: Todo["id"]): Todo[] => {
+  return todos.filter((todo) => todo.id !== id);
+};
+
+const handleAddTodo = (todos: Todo[], text: Todo["text"]): Todo[] => {
+  return [...todos, { id: uuid(), text, completed: false }];
+};
